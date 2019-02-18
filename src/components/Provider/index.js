03e55a2c2f6e72da48e93c97f46924614a605baa
Elliott Humphreys <elliott.humphreys@stockport.gov.uk>
2019-02-18 08:59:32 +0000

    refactor(most things): changed provider to one onChange, useContext hook on example page

--- src/components/Provider/index.js
+++ src/components/Provider/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import moment from 'moment'
 import { Context } from '../../context/'
 
 class Provider extends Component{
@@ -13,50 +12,16 @@ class Provider extends Component{
 			},
 			displayRecaptcha: document.getElementById('displayRecaptcha') != null ? document.getElementById('displayRecaptcha').innerHTML === 'true' ? true : false : false,
 			onChange: this.onChange,
-			onAddressChange: this.onAddressChange,
-			onChangePaymentDate: this.onChangePaymentDate,
-			onCheckBoxChange: this.onCheckBoxChange,
 			onFormSubmission: this.onFormSubmission
 		}
 	}
 	
 	onChange = (event, isValid) => {
-		const copyOfState = Object.assign({}, this.state)
-		copyOfState[event.target.name].value = event.target.value
-		copyOfState[event.target.name].isValid = isValid
-
-		this.setState(copyOfState)
-	}
-
-	onAddressChange = (isValid, addressDetails, ) => {
-		const copyOfState = Object.assign({}, this.state)
-		copyOfState.address.value = addressDetails
-		copyOfState.address.isValid = isValid
-
-		this.setState(copyOfState)
-	}
-
-	onCheckBoxChange = (event, isValid) => {
-		const copyOfState = Object.assign({}, this.state)
-		copyOfState[event.target.name].value = event.target.checked
-		copyOfState[event.target.name].isValid = isValid
-
-		this.setState(copyOfState)
-	}
-
-	onChangePaymentDate = (event, isValid, applicationDate) => {
-		const copyOfState = Object.assign({}, this.state)
-		copyOfState[event.target.name].value = event.target.value
-		copyOfState[event.target.name].isValid = isValid
-
-		copyOfState.applicationDate = applicationDate
-		copyOfState.firstPaymentDate = moment(copyOfState.applicationDate).date(copyOfState.paymentDate.value)
-
-		for(let i=0; copyOfState.firstPaymentDate.diff(copyOfState.applicationDate, 'days') < 14 || copyOfState.firstPaymentDate.format('DDMMMM') == '01January'; i++){
-			copyOfState.firstPaymentDate = copyOfState.firstPaymentDate.add(1, 'month')
-		}
-		
-		this.setState(copyOfState)
+		this.setState({
+			[event.target.name]: {
+				value: event.target.name, 
+				isValid
+			}})
 	}
 
 	onFormSubmission = () => {
